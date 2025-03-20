1a9b8038f63ce5b4cb4e955d092f5c6c75a48601
Kirill Kuznetsov <kdevlab@yandex.ru>
2025-03-20 15:16:22 +0300

    Add vscode updater

--- flake.nix
+++ flake.nix
@@ -4,10 +4,30 @@
   inputs = {
     nixpkgs.url = "github:miuirussia/nixpkgs/nixpkgs-unstable";
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+
+    systems.url = "github:nix-systems/default-linux";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    {
+  outputs = { nixpkgs, systems, ... }@inputs:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = lib.genAttrs (import systems);
+    in {
+      apps = eachSystem (system: {
+        update = with nixpkgs.legacyPackages.${system}; {
+          type = "app";
+          program = let
+            script = writeShellScript "update" ''
+              set -euo pipefail
+              export PATH=${lib.makeBinPath [ gitMinimal jq nixVersions.git hostname node2nix ]}
+              export NIX_PATH=nixpkgs=${inputs.nixpkgs}
+              ./update-vscode-plugins.py
+              ./codelldb/update.sh
+            '';
+          in "${script}";
+        };
+      });
+
       overlays.default =
         final: prev: with builtins; rec {
           vscode-custom-extensions-all = prev.lib.attrsets.attrValues vscode-custom-extensions;
