a66436ff03da1f88e1c03419400cb7711d8f8a08
Kirill Kuznetsov <kdevlab@yandex.ru>
2025-09-16 17:36:10 +0300

    Bump continue

--- continue/default.nix
+++ continue/default.nix
@@ -2,9 +2,9 @@
 
 pkgs.vscode-utils.buildVscodeMarketplaceExtension rec {
   vsix = builtins.fetchurl {
-    name = "continue-1.3.2.zip";
-    url = "https://github.com/miuirussia/continue/releases/download/v1.3.2/continue-linux-x64-1.3.2.vsix";
-    sha256 = "4500e666c9b954d29ce6ba46207d5025a3d59381761d120a8ad86c1cb39718b3";
+    name = "continue.zip";
+    url = "https://github.com/miuirussia/continue/releases/download/v1.4.41/continue-1.3.8-60fc293.vsix";
+    sha256 = "aa02d7d046e63c9ff9fcd3f6a997e8747d28786f77eb1341066ccab2978c9601";
   };
 
   nativeBuildInputs = with pkgs; [
@@ -17,6 +17,6 @@ pkgs.vscode-utils.buildVscodeMarketplaceExtension rec {
   mktplcRef = {
     publisher = "Continue";
     name = "continue";
-    version = "1.3.2";
+    version = "1.3.8";
   };
 }
